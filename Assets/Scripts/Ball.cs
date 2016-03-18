d16bc0684608cabac06cf9429088fd371f2de366
Liam Wilson <wilsoncgp@gmail.com>
2016-03-18 01:32:50 +0000

    Added moving bowling ball
    Added the ball with an initial velocity and sound.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    public float initialSpeed;
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+        audioSource = this.GetComponent<AudioSource>();
+
+        Launch();
+    }
+
+    public void Launch()
+    {
+        rigidBody.velocity = Vector3.forward * initialSpeed;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
