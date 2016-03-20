f8c9f4457548eeecaa4b657a172e82098fcd57d5
Liam Wilson <wilsoncgp@gmail.com>
2016-03-20 03:04:57 +0000

    Added Pin script to check if Pin is standing
    A slight alteration from the course's way of checking if a pin is
    upright. Keeping with the threshold of angles but providing a more
    accurate angle using the Vector3.Angle() function with the world Up
    vector and the Pin's up vector, rather than checking the threshold
    against the rotation in X and Z separately.

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour
+{
+    public float standingThreshold = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool IsStanding()
+    {
+        return Mathf.Abs(Vector3.Angle(Vector3.up, this.transform.up)) < standingThreshold;
+    }
+}
