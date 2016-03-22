9230cd4c318ba9746d74d749061865ea77489711
Liam Wilson <wilsoncgp@gmail.com>
2016-03-22 20:26:32 +0000

    Added Pin Setter to update standing pins
    Basic Pin Setter added with a UI element to update the user on the
    number of standing pins based on IsStanding() method developed in one of
    the previous commits.

---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PinSetter : MonoBehaviour
+{
+    public Text pinNumberText;
+
+    private Pin[] pins;
+
+    // Use this for initialization
+    void Start()
+    {
+        pins = GameObject.FindObjectsOfType<Pin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pinNumberText)
+        {
+            pinNumberText.text = CountStanding().ToString();
+        }
+    }
+
+    // Count the standing pins
+    int CountStanding()
+    {
+        int count = 0;
+        foreach(Pin pin in pins)
+        {
+            count += pin.IsStanding() ? 1 : 0;
+        }
+
+        return count;
+    }
+}
