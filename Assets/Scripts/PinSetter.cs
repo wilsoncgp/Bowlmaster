896c888b2d1c3b2f3934d3f51b949a825a6fa602
Liam Wilson <wilsoncgp@gmail.com>
2016-03-23 02:58:31 +0000

    Added trigger actions for Ball entering and Pins exiting
    Ball will now trigger an action when entering the Pin Setter, to change
    the text colour of the number of remaining pins to red.
    
    When the pins exit the Pin Setter, they will be destroyed.

--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -5,33 +5,47 @@ using System.Collections;
 public class PinSetter : MonoBehaviour
 {
     public Text pinNumberText;
-
-    private Pin[] pins;
+    
+    private bool ballEnteredBox = false;
 
     // Use this for initialization
     void Start()
     {
-        pins = GameObject.FindObjectsOfType<Pin>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(pinNumberText)
-        {
-            pinNumberText.text = CountStanding().ToString();
-        }
+        pinNumberText.text = CountStanding().ToString();
     }
 
     // Count the standing pins
     int CountStanding()
     {
         int count = 0;
-        foreach(Pin pin in pins)
+        foreach (Pin pin in GameObject.FindObjectsOfType<Pin>())
         {
             count += pin.IsStanding() ? 1 : 0;
         }
 
         return count;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.GetComponent<Ball>())
+        {
+            pinNumberText.color = Color.red;
+            ballEnteredBox = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        Pin pin = collider.GetComponentInParent<Pin>();
+        if(pin)
+        {
+            Destroy(pin.gameObject);
+        }
     }
 }
