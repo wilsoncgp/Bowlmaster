68a53aaeb06c4731961e3bf7e2a1efa39d423674
Liam Wilson <wilsoncgp@gmail.com>
2016-03-29 01:05:42 +0100

    Started test-driven development to implement the scoring system
    Starting with a simple test to check if a strike called in any turn will
    return the Action to End a turn.

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+[TestFixture]
+public class ActionMasterTest
+{
+    private ActionMaster actionMaster = new ActionMaster();
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+    [Test]
+    public void PassingTest()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [Test]
+    public void FirstStrikeReturnsEndTurn()
+    {
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+}
---
+++ Assets/Scripts/Standard/ActionMaster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMaster
+{
+
+    public enum Action { Tidy, Reset, EndTurn, EndGame }
+
+    public Action Bowl(int pins)
+    {
+        if(pins < 0)
+        {
+            throw new UnityException("Number of pins cannot be less than 0.");
+        }
+        else if(pins > 10)
+        {
+            throw new UnityException("Number of pins cannot be greater than 10.");
+        }
+
+        // Bowled a strike
+        if(pins == 10)
+        {
+            return Action.EndTurn;
+        }
+
+        throw new UnityException("No action specified by given pin count.");
+    }
+}
