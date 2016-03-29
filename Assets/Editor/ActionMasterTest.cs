8b2f90f28feb0c58d3a6b8b1df4f2dea3e9f732d
Liam Wilson <wilsoncgp@gmail.com>
2016-03-29 05:14:39 +0100

    Many more tests, much more secure ActionMaster
    Really nailing down on adding a lot of tests, particularly those
    relating to the last frame in a game of bowling and whether we need to
    Tidy the pins, Reset the pins or End the Game. As the ActionMaster code
    itself shows, the main issue is surrounding the 20th Bowl when either of
    the 3 conditions can occur for different reasons.
    
    Also fixing a bug with a 0,10 spare in a frame causing the next less
    than 10 bowl to be considered the End of a Turn rather than a Tidy.

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -5,18 +5,223 @@ using NUnit.Framework;
 [TestFixture]
 public class ActionMasterTest
 {
-    private ActionMaster actionMaster = new ActionMaster();
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
 
-    [Test]
-    public void PassingTest()
+    private ActionMaster actionMaster;
+
+    [SetUp]
+    public void Setup()
     {
-        Assert.IsTrue(true);
+        actionMaster = new ActionMaster();
     }
 
     [Test]
     public void FirstStrikeReturnsEndTurn()
     {
         Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void BowlEightReturnsTidy()
+    {
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void BowlSpareReturnsEndTurn()
+    {
+        int firstBowl = Random.Range(0, 10);
+
+        Assert.AreEqual(tidy, actionMaster.Bowl(firstBowl));
+
+        int secondBowl = 10 - firstBowl;
+
+        Assert.AreEqual(endTurn, actionMaster.Bowl(secondBowl));
+    }
+
+    [Test]
+    public void BowlSpareWhenMissFirstBowlReturnsEndTurn()
+    {
+        actionMaster.Bowl(0);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void BowlSpareWhenMissFirstBowlThenBowlLessThanTenReturnsTidy()
+    {
+        actionMaster.Bowl(0);
+        actionMaster.Bowl(10);
+        Assert.AreEqual(tidy, actionMaster.Bowl(3));
+    }
+
+    [Test]
+    public void BowlTwiceNoStrikeNoSpareReturnsEndTurn()
+    {
+        int firstBowl = Random.Range(0, 9);
+
+        Assert.AreEqual(tidy, actionMaster.Bowl(firstBowl));
+
+        int secondBowl = 9 - firstBowl;
+
+        Assert.AreEqual(endTurn, actionMaster.Bowl(secondBowl));
+    }
+
+    [Test]
+    public void BowlThreeInFirstBowlOfLastFrameReturnsTidy()
+    {
+        BowlToLastFrame(ref actionMaster);
+
+        Assert.AreEqual(tidy, actionMaster.Bowl(3));
+    }
+
+    [Test]
+    public void BowlThreeAndSevenInFirstAndSecondBowlsOfLastFrameReturnsReset()
+    {
+        BowlToLastFrame(ref actionMaster);
+
+        actionMaster.Bowl(3);
+        Assert.AreEqual(reset, actionMaster.Bowl(7));
+    }
+
+    [Test]
+    public void BowlStrikeInFirstBowlOfLastFrameReturnsReset()
+    {
+        BowlToLastFrame(ref actionMaster);
+
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void BowlStrikeInFirstAndSecondBowlOfLastFrameReturnsReset()
+    {
+        BowlToLastFrame(ref actionMaster);
+
+        actionMaster.Bowl(10);
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void BowlStrikeInAllThreeBowlsOfLastFrameReturnsEndGame()
+    {
+        BowlToLastFrame(ref actionMaster);
+
+        actionMaster.Bowl(10);
+        actionMaster.Bowl(10);
+        Assert.AreEqual(endGame, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void BowlTwelveStrikesReturnsEndGame()
+    {
+        for(int i = 0; i < 11; i++)
+        {
+            actionMaster.Bowl(10);
+        }
+
+        Assert.AreEqual(endGame, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void BowlStrikeThenZeroInLastFrameReturnsTidy()
+    {
+        BowlToLastFrame(ref actionMaster);
+
+        actionMaster.Bowl(10);
+        Assert.AreEqual(tidy, actionMaster.Bowl(0));
+    }
+
+    [Test]
+    public void BowlStrikeThenSpareInLastFrameReturnsEndGame()
+    {
+        BowlToLastFrame(ref actionMaster);
+
+        actionMaster.Bowl(10);
+        actionMaster.Bowl(3);
+        Assert.AreEqual(endGame, actionMaster.Bowl(7));
+    }
+
+    [Test]
+    public void BowlTwentyTimesWithNoStrikesOrSparesReturnsEndGame()
+    {
+        for(int i = 0; i < 19; i++)
+        {
+            actionMaster.Bowl(1);
+        }
+
+        Assert.AreEqual(endGame, actionMaster.Bowl(1));
+    }
+
+    [Test]
+    public void BowlFullGameThenBowlThreeReturnsTidy()
+    {
+        BowlFullGame(ref actionMaster);
+
+        Assert.AreEqual(tidy, actionMaster.Bowl(3));
+    }
+
+    [Test]
+    public void BowlFullGameThenBowlSpareReturnsEndTurn()
+    {
+        BowlFullGame(ref actionMaster);
+
+        actionMaster.Bowl(5);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(5));
+    }
+
+    [Test]
+    public void BowlFullGameThenBowlStrikeReturnsEndTurn()
+    {
+        BowlFullGame(ref actionMaster);
+
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void BowlFullGameThenBowlStrikeInLastFrameReturnsReset()
+    {
+        BowlFullGame(ref actionMaster);
+        BowlToLastFrame(ref actionMaster);
+
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void BowlFullGameThenBowlSpareInLastFrameReturnsReset()
+    {
+        BowlFullGame(ref actionMaster);
+        BowlToLastFrame(ref actionMaster);
+
+        actionMaster.Bowl(3);
+        Assert.AreEqual(reset, actionMaster.Bowl(7));
+    }
+
+    [Test]
+    public void BowlFullGameThenBowlThreeStrikesInLastFrameReturnsEndGame()
+    {
+        BowlFullGame(ref actionMaster);
+        BowlToLastFrame(ref actionMaster);
+
+        actionMaster.Bowl(10);
+        actionMaster.Bowl(10);
+        Assert.AreEqual(endGame, actionMaster.Bowl(10));
+    }
+
+    private void BowlToLastFrame(ref ActionMaster aMaster)
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            aMaster.Bowl(10);
+        }
+    }
+
+    private void BowlFullGame(ref ActionMaster aMaster)
+    {
+        for(int i = 0; i < 12; i++)
+        {
+            aMaster.Bowl(10);
+        }
     }
 }
--- Assets/Scripts/Standard/ActionMaster.cs
+++ Assets/Scripts/Standard/ActionMaster.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class ActionMaster
 {
-
     public enum Action { Tidy, Reset, EndTurn, EndGame }
+
+    private int[] bowls = new int[21];
+    private int bowl = 1;
 
     public Action Bowl(int pins)
     {
+        // Throw exception if an invalid number of pins for a bowl
         if(pins < 0)
         {
             throw new UnityException("Number of pins cannot be less than 0.");
@@ -17,12 +20,87 @@ public class ActionMaster
             throw new UnityException("Number of pins cannot be greater than 10.");
         }
 
-        // Bowled a strike
-        if(pins == 10)
+        // Store value for bowl
+        bowls[bowl - 1] = pins;
+        
+        // If first bowl of last frame and bowled a strike
+        //  it must reset the pins
+        if(bowl == 19 && pins == 10)
         {
+            bowl++;
+            return Action.Reset;
+        }
+
+        // If it's the second bowl of the last frame
+        if(bowl == 20)
+        {
+            // If a strike or 0,10 spare was bowled, reset the pins
+            if(pins == 10)
+            {
+                bowl++;
+                return Action.Reset;
+            }
+            // If a strike was bowled in previous bowl
+            //  but not this bowl, tidy the pins
+            else if (bowls[bowl - 2] == 10)
+            {
+                bowl++;
+                return Action.Tidy;
+            }
+            // If a strike wasn't bowled in this or the last frame,
+            //  but a spare was bowled across the first and second bowl
+            //  of the last frame, reset the pins
+            else if(bowls[bowl - 2] + pins == 10)
+            {
+                bowl++;
+                return Action.Reset;
+            }
+            else 
+            {
+                ResetBowls();
+                return Action.EndGame;
+            }
+        }
+
+        // If third bowl of final frame, must be final bowl
+        //  so end the game
+        if(bowl == 21)
+        {
+            ResetBowls();
+            return Action.EndGame;
+        }
+        
+        // If in first bowl of frame
+        if(bowl % 2 == 1)
+        {
+            // Bowled a strike
+            if(pins == 10)
+            {
+                bowl += 2;
+                return Action.EndTurn;
+            }
+            else
+            {
+                bowl++;
+                return Action.Tidy;
+            }
+        }
+        else if(bowl % 2 == 0)
+        {
+            bowl++;
             return Action.EndTurn;
         }
 
-        throw new UnityException("No action specified by given pin count.");
+        throw new UnityException("No action specified by given pin count for specific bowl.");
+    }
+
+    /// <summary>
+    /// Reset the bowls for the game.
+    /// Should be called when game ends.
+    /// </summary>
+    private void ResetBowls()
+    {
+        bowls = new int[21];
+        bowl = 1;
     }
 }
