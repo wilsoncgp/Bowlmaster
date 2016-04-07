93a16a96570c978ada3eced933bb15ad54436e17
Liam Wilson <wilsoncgp@gmail.com>
2016-04-07 05:06:36 +0100

    Wiring up the Score Display to the UI
    Added public Text arrays to drag the UI Text Elements into

---
+++ Assets/Scripts/MonoBehaviours/ScoreDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public Text[] rollScoreTexts;
+    public Text[] frameScoreTexts;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
